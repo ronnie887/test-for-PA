44668e18641dc76856db39da3c6b0d7724bbe356
Rohit Chamle <rohitchamle@outlook.com>
2025-09-05 13:18:03 +0530

    updated

--- main.tf
+++ main.tf
@@ -21,13 +21,13 @@ resource "azurerm_resource_group" "main" {
 
 ## Storage Account (ADLS Gen2)
 resource "azurerm_storage_account" "datalake" {
-  name = azurepasa
+  name = "azurepasa"
   resource_group_name = azurerm_resource_group.main.name
   location = azurerm_resource_group.main.location
   account_tier = "Standard"
   account_replication_type = "LRS"
   account_kind = "StorageV2"
-  enable_https_traffic_only = true
+  #enable_https_traffic_only = true
   is_hns_enabled = true # Required for ADLS Gen2
 }
 
