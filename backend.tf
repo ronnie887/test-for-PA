fb24eba21b549d8b9a37b9b4700a56e93d76513c
Rohit Chamle <rohitchamle@outlook.com>
2025-09-10 18:00:11 +0530

    deleted backend

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name = "rg-tfstate"
-    storage_account_name = "tfstatesapa"
-    container_name = "tfstate"
-    key = "test.terraform.tfstate"
-  }
-}
