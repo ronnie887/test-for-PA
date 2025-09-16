36c643c3534cf45fa1363dc8555147a1a21c22f6
Rohit Chamle <rohitchamle@outlook.com>
2025-09-16 14:37:03 +0530

    removed the qoutes

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "azurerm_key_vault" "kv" {
   name = "${var.key_vault_name}-${var.app_name}-${var.environment}"
   resource_group_name = azurerm_resource_group.rg.name
   location = azurerm_resource_group.rg.location
-  tenant_id = "data.azurerm_client_config.current.tenant_id"
+  tenant_id = data.azurerm_client_config.current.tenant_id
   sku_name = "standard"
 
   access_policy {
