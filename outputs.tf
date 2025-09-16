913665a1501c0e8dedd78455a4c22f936d9c8566
Rohit Chamle <rohitchamle@outlook.com>
2025-09-16 13:47:37 +0530

    updated tf files to deploy more resources

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,85 @@
+output "resource_group_name" {
+  description = "Name of the created resource group"
+  value       = azurerm_resource_group.rg.name
+}
+
 output "adls_storage_account_name" {
-  value = azurerm_storage_account.adls.name
+  description = "ADLS Gen2 storage account name"
+  value       = azurerm_storage_account.adls.name
+}
+
+output "adls_storage_account_primary_endpoint" {
+  description = "Primary DFS endpoint of the ADLS Gen2 account"
+  value       = azurerm_storage_account.adls.primary_dfs_endpoint
+}
+
+output "key_vault_name" {
+  description = "Name of the Azure Key Vault"
+  value       = azurerm_key_vault.kv.name
+}
+
+output "key_vault_uri" {
+  description = "URI of the Azure Key Vault"
+  value       = azurerm_key_vault.kv.vault_uri
+}
+
+output "postgres_server_fqdn" {
+  description = "Fully qualified domain name of the PostgreSQL server"
+  value       = azurerm_postgresql_flexible_server.postgres.fqdn
+}
+
+output "postgres_database_name" {
+  description = "Name of the PostgreSQL database"
+  value       = azurerm_postgresql_flexible_server_database.app_db.name
+}
+
+output "container_registry_login_server" {
+  description = "Login server of the Azure Container Registry"
+  value       = azurerm_container_registry.acr.login_server
+}
+
+output "container_registry_admin_username" {
+  description = "Admin username for the Azure Container Registry"
+  value       = azurerm_container_registry.acr.admin_username
+  sensitive   = true
+}
+
+output "web_app_url" {
+  description = "URL of the deployed Linux Web App"
+  value       = "https://${azurerm_linux_web_app.web_app.default_hostname}"
+}
+
+output "web_app_name" {
+  description = "Name of the Linux Web App"
+  value       = azurerm_linux_web_app.web_app.name
+}
+
+output "data_factory_name" {
+  description = "Name of the Azure Data Factory"
+  value       = azurerm_data_factory.adf.name
+}
+
+output "data_factory_identity_principal_id" {
+  description = "Principal ID of the Data Factory managed identity"
+  value       = azurerm_data_factory.adf.identity[0].principal_id
+}
+
+output "app_service_plan_name" {
+  description = "Name of the App Service Plan"
+  value       = azurerm_service_plan.app_plan.name
+}
+
+output "azure_ad_application_id" {
+  description = "Application (client) ID of the Azure AD application"
+  value       = azuread_application.app.application_id
+}
+
+output "user_assigned_identity_id" {
+  description = "ID of the User-Assigned Managed Identity"
+  value       = azurerm_user_assigned_identity.uami_core.id
+}
+
+output "user_assigned_identity_principal_id" {
+  description = "Principal ID of the User-Assigned Managed Identity"
+  value       = azurerm_user_assigned_identity.uami_core.principal_id
 }
--- variables.tf
+++ variables.tf
@@ -21,3 +21,65 @@ variable "environment" {
   type        = string
   default     = "dev"
 }
+
+variable "key_vault_name" {
+  description = "Name of the Azure Key vault "
+  type = string
+  default = "kv"
+}
+
+variable "postgres_server_name" {
+  description = "Name of the PostgreSQL server"
+  type = string
+  default = "postgres-server"
+}
+
+variable "postgres_admin_username" {
+  description = "Administrator username for PostgreSQL"
+  type        = string
+  default     = "pgadmin-integriX"
+  sensitive   = true
+}
+
+variable "postgres_admin_password" {
+  description = "Administrator password for PostgreSQL"
+  type        = string
+  sensitive   = true
+  default     = "P@ssw0rd123!" # Change this in production!
+}
+
+variable "postgres_database_name" {
+  description = "Name of the PostgreSQL database"
+  type        = string
+  default     = "db"
+}
+
+variable "container_registry_name" {
+  description = "Name of the Azure Container Registry (prefix)"
+  type        = string
+  default     = "acr"
+}
+
+variable "app_service_plan_name" {
+  description = "Name of the App Service Plan"
+  type        = string
+  default     = "asp"
+}
+
+variable "web_app_name" {
+  description = "Name of the Web App (prefix)"
+  type        = string
+  default     = "webapp"
+}
+
+variable "data_factory_name" {
+  description = "Name of the Azure Data Factory (prefix)"
+  type        = string
+  default     = "adf"
+}
+
+variable "app_name" {
+  description = "Name of the Azure AD application"
+  type        = string
+  default     = "integriX"
+}
