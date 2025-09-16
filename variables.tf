840a298cf03f50ec59980d88b5f41cbc949bf650
Rohit Chamle <rohitchamle@outlook.com>
2025-09-16 17:56:10 +0530

    location changed to centralus

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "location" {
   description = "The Azure region where resources will be created"
   type        = string
-  default     = "East US"
+  default     = "centralus"
 }
 
 variable "resource_group_name" {
