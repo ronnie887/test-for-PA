9d7226db2b1a47df6bb30f506bac9055cd11521c
Rohit Chamle <rohitchamle@outlook.com>
2025-10-08 17:35:01 +0530

    dummy uami

--- config_files/iam/uami/data.tf
+++ config_files/iam/uami/data.tf
@@ -1,6 +1,6 @@
 data "terraform_remote_state" "resource_group" {
   backend = "azurerm"
   config = {
-    key = "config_files/resource_group/terraform.tfstate"
+    key = "config_files/resource_group/resource_group/terraform.tfstate"
   }
 }
