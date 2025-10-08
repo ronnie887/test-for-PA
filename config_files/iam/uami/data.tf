edcc21ef72a711f2d8c9bb71496c687444b3c720
Rohit Chamle <rohitchamle@outlook.com>
2025-10-08 17:40:44 +0530

    dummy uami

--- config_files/iam/uami/data.tf
+++ config_files/iam/uami/data.tf
@@ -1,8 +1,8 @@
 data "terraform_remote_state" "resource_group" {
   backend = "azurerm"
   config = {
-    resource_group_name  = "your-tfstate-rg"
-    storage_account_name = "yourtfstatestorage"
+    resource_group_name  = "pa-tfstate-rg"
+    storage_account_name = "patfstatestorage"
     container_name       = "tfstate"
     key = "config_files/resource_group/resource_group/terraform.tfstate"
   }
