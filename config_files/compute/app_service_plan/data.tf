4be13cc4920cb4d1d4d373a6da65a83bf5c57875
Rohit Chamle <rohitchamle@outlook.com>
2025-10-08 18:07:25 +0530

    dummy rest services

--- config_files/compute/app_service_plan/data.tf
+++ config_files/compute/app_service_plan/data.tf
@@ -4,6 +4,6 @@ data "terraform_remote_state" "resource_group" {
     resource_group_name  = "pa-tfstate-rg"
     storage_account_name = "patfstatestorage"
     container_name       = "tfstate"
-    key = "config_files/resource_group/terraform.tfstate"
+    key = "config_files/resource_group/resource_group/terraform.tfstate"
   }
 }
--- config_files/iam/key_vault/data.tf
+++ config_files/iam/key_vault/data.tf
@@ -4,7 +4,7 @@ data "terraform_remote_state" "resource_group" {
     resource_group_name  = "pa-tfstate-rg"
     storage_account_name = "patfstatestorage"
     container_name       = "tfstate"
-    key = "config_files/resource_group/terraform.tfstate"
+    key = "config_files/resource_group/resource_group/terraform.tfstate"
   }
 }
 
