ea724c88177170ab2750311c4c6711c9af1ffd74
Rohit Chamle <rohitchamle@outlook.com>
2025-10-15 12:41:48 +0530

    dummy rg added var in the .tf file

--- test/resource_group/resource_group/variables.tf
+++ test/resource_group/resource_group/variables.tf
@@ -1,16 +1,19 @@
 variable "project_name" {
   description = "The name of the project"
   type        = string
+  default = ""
 }
 
 variable "environment" {
   description = "Environment name"
   type        = string
+  default = ""
 }
 
 variable "location" {
   description = "Azure region for resources"
   type        = string
+  default = ""
 }
 
 variable "tags" {
