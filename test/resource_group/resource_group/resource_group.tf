21f58a5358655578415bebca09def2b2f7b4ddb2
Rohit Chamle <rohitchamle@outlook.com>
2025-10-15 13:03:51 +0530

    rg test

--- test/resource_group/resource_group/resource_group.tf
+++ test/resource_group/resource_group/resource_group.tf
@@ -24,7 +24,7 @@ terraform {
 module "resource_group" {
   source = "../../../modules/resource_group"
   
-  name         = "rg-pa-integrix-test1"
+  name         = "rg-pa-integrix-test2"
   project_name = "pa-integrix"
   environment  = "test"
   location     = "centralus"
